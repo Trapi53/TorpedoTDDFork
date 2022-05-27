1d2c366942612af72af96189101fcf816be7c3c9
Trapi53 <trapi73@gmail.com>
2022-05-27 12:25:09 +0200

    LovesNemTalalt()sikeres

--- src/teszt/TorpedoTeszt.java
+++ src/teszt/TorpedoTeszt.java
@@ -5,15 +5,24 @@ import model.Hajo;
 public class TorpedoTeszt {
 
     public static void main(String[] args) {
-        System.out.println("Teszt");
+        System.out.println("Tesztesetek");
 
         new TorpedoTeszt().tesztLovesTalalt(4);
+        new TorpedoTeszt().LovesNemTalalt(5);
     }
 
     public String tesztLovesTalalt(int poz) {
         Hajo hajo = new Hajo(new int[]{2, 3, 4});
         String t = hajo.talalat(poz);
         assert t.equals("talált") : "nem jó a találat ellenőrzése";
+        return "";
+    }
+    public String LovesNemTalalt(int poz) {
+        System.out.println("----Mellé");
+        Hajo hajo = new Hajo(new int[]{2, 3, 4});
+        String t = hajo.talalat(poz);
+        assert t.equals("nem talált") : "nem jó a találat ellenőrzése";
+        System.out.println("Mellé teszt sikeres");
         return "";
     }
 }
